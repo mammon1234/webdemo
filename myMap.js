6dd4b4b5e88fa71cc469fef2e8baf8a51ab1deb5
Rocky  Luo <rocky.luo@rutgers.edu>
2016-11-13 23:42:59 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AtzhLS1Zy7h39DFuvbNJWCFq0j7GtzW2uQvuIdzwGWk0sBpXu95RQs8Rx2pMAHdX'
     });
 }
