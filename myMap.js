289b590ee17b40c358a8f43cd3deb88ff92d5293
Rocky  Luo <rocky.luo@rutgers.edu>
2016-11-14 01:59:34 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,4 +1,7 @@
-var map;
+var map_manager = {
+    "map" : null,
+    "map_items" : []
+}
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AtzhLS1Zy7h39DFuvbNJWCFq0j7GtzW2uQvuIdzwGWk0sBpXu95RQs8Rx2pMAHdX'
