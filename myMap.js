9fc48fa41252da0af38d3bd30e09bb3d89b08c08
Rocky  Luo <rocky.luo@rutgers.edu>
2016-11-14 23:46:31 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -28,7 +28,7 @@ function get_counter_down_time_from_expire_epoch(epoch) {
   var minute = Math.floor(time_left / 60);
 
     
-  return time_left + ":" + second;
+  return minute + ":" + second;
 }
 
 // 2. Create pokemon image on map
